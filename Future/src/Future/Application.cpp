e47c57e9653eca773ae5f727a07f7d8c3bfa30d8
halilyaman <halilymn03@gmail.com>
2021-03-27 12:24:43 +0300

    Setup basic triangle

--- Future/src/Future/Application.cpp
+++ Future/src/Future/Application.cpp
@@ -8,6 +8,117 @@ namespace Future
 {
 	Application* Application::s_Instance = nullptr;
 
+	void setupTriangle()
+	{
+		float vertices[] =
+		{
+			 0.0f,  0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			-0.5f, -0.5f, 0.0f
+		};
+
+		unsigned int vbo;
+		glGenBuffers(1, &vbo);
+		FT_CORE_TRACE("Created buffer object (unbounded)");
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		FT_CORE_TRACE("Bound buffer object");
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		FT_CORE_TRACE("Set data to currently bound buffer object");
+
+		const char* vertexShaderSource =
+			"#version 330 core\n"
+			"layout (location = 0) in vec3 pos;\n"
+			"void main()\n"
+			"{\n"
+			"	gl_Position = vec4(pos, 1.0);\n"
+			"}\n";
+
+		unsigned int vertexShader;
+		vertexShader = glCreateShader(GL_VERTEX_SHADER);
+		FT_CORE_TRACE("Created vertex shader");
+		glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+		FT_CORE_TRACE("Set source code of vertex shader");
+		glCompileShader(vertexShader);
+		FT_CORE_TRACE("Compiled vertex shader");
+
+		int vertexCompiled;
+		char infoLog[512];
+		glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vertexCompiled);
+		if (!vertexCompiled)
+		{
+			int logLength = 0;
+			glGetShaderInfoLog(vertexShader, 512, &logLength, infoLog);
+			std::stringstream ss;
+			ss << "VERTEX SHADER COMPILE ERROR: " << infoLog;
+			FT_CORE_ERROR(ss.str());
+		}
+		else
+		{
+			FT_CORE_TRACE("Vertex shader compiled successfully");
+		}
+
+		const char* fragmentShaderSource =
+			"#version 330 core\n"
+			"out vec4 color;\n"
+			"void main()\n"
+			"{\n"
+			"	color = vec4(0.0f, 0.5f, 0.5f, 1.0f);\n"
+			"}\n";
+
+		unsigned int fragmentShader;
+		fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+		FT_CORE_TRACE("Created fragment shader");
+		glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+		FT_CORE_TRACE("Set source code of fragment shader");
+		glCompileShader(fragmentShader);
+		FT_CORE_TRACE("Compiled fragment shader");
+
+		int fragmentCompiled;
+		glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &fragmentCompiled);
+		if (!fragmentCompiled)
+		{
+			int logLength = 0;
+			glGetShaderInfoLog(fragmentShader, 512, &logLength, infoLog);
+			std::stringstream ss;
+			ss << "FRAGMENT SHADER COMPILE ERROR: " << infoLog;
+			FT_CORE_ERROR(ss.str());
+		}
+		else
+		{
+			FT_CORE_TRACE("Fragment shader compiled successfully");
+		}
+
+		unsigned int triangleShaderProgram;
+		triangleShaderProgram = glCreateProgram();
+		FT_CORE_TRACE("Created Triangle Shader Program");
+		glAttachShader(triangleShaderProgram, vertexShader);
+		glAttachShader(triangleShaderProgram, fragmentShader);
+		FT_CORE_TRACE("Attached vertex and fragment shaders to the Triangle Shader Program");
+		glLinkProgram(triangleShaderProgram);
+
+		int programLinked;
+		glGetProgramiv(triangleShaderProgram, GL_LINK_STATUS, &programLinked);
+		if (!programLinked)
+		{
+			int logLength = 0;
+			glGetProgramInfoLog(triangleShaderProgram, 512, &logLength, infoLog);
+			std::stringstream ss;
+			ss << "SHADERS LINK ERROR: " << infoLog;
+			FT_CORE_ERROR(ss.str());
+		}
+		else
+		{
+			FT_CORE_TRACE("Triangle Shader Program linked successfully");
+			glDeleteShader(vertexShader);
+			glDeleteShader(fragmentShader);
+		}
+
+		glUseProgram(triangleShaderProgram);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), 0);
+		glEnableVertexAttribArray(0);
+	}
+
 	Application::Application()
 	{
 		FT_CORE_ASSERT(!s_Instance, "Application already exists!");
@@ -25,6 +136,8 @@ namespace Future
 		// add ImGuiLayer to the layer stack
 		m_ImGuiLayer = new ImGuiLayer();
 		PushOverlay(m_ImGuiLayer);
+
+		setupTriangle();
 	}
 
 	Application::~Application()
@@ -38,6 +151,8 @@ namespace Future
 		{
 			glClearColor(0.2f, 0.0f, 0.1f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT);
+
+			glDrawArrays(GL_TRIANGLES, 0, 3);
 
 			for (Layer* layer : m_LayerStack)
 			{
